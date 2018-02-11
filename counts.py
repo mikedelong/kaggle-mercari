921f8ebd16da1b699ac86e23acf1b1f6df8e8529
Mike DeLong <mikedelong@outlook.com>
2018-02-11 12:08:15 -0500

    feature: in this little bit of code we are only interested in the training data, so let's not load the test data for the moment.

--- counts.py
+++ counts.py
@@ -1,16 +1,8 @@
-import gc
 import logging
 import re
 import time
 
-import lightgbm as lgbm
-import numpy as np
 import pandas as pd
-from scipy import sparse as ssp
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
 
 start_time = time.time()
 
@@ -50,11 +42,6 @@ logger.debug('loading training data from %s' % full_train_file)
 encoding = 'utf-8'
 train = pd.read_csv(full_train_file, sep="\t", encoding=encoding, converters=converters)
 logger.debug('training data load complete.')
-test_file = 'test.tsv'
-full_test_file = input_folder + test_file
-logger.debug('loading test data from %s' % full_test_file)
-test = pd.read_csv(full_test_file, sep="\t", encoding=encoding, converters=converters)
-logger.debug('test data load complete.')
 
 
 finish_time = time.time()
